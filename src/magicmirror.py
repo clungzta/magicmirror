ed171770862597617e6c224830aec85d031bc064
Alex McClung <alex.mcclung@hotmail.com>
2017-08-16 14:36:32 +1000

    initial commit

---
+++ src/magicmirror.py
@@ -0,0 +1,13 @@
+from speech_utils import *
+from vision_utils import *
+
+class MagicMirror:
+    def __init__():
+        print('Initializing Magic Mirror...')
+
+
+    def process_frame(image):
+        print('reading frame!')
+
+
+if __name__ == "__main__":
